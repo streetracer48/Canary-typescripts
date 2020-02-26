8be3ae9d27fedd4cc18d87b15c8c2c80c40cbb28
karim <karim@csit.fi>
2020-02-26 17:48:38 +0200

    methodvalidation

---
+++ fetchjson/methodvalidation.ts
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+
+const url ='https://jsonplaceholder.typicode.com/todos/1'
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then(response =>{
+    const todo = response.data as Todo
+
+    const ID = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+  logTodo(ID,title, completed)     
+})
+
+const logTodo = ( id:number, title:string, completed:boolean) => {
+
+
+    console.log(`The todo with TD:${id}
+    Has a title of:${title}
+    Is it finished ? ${completed}
+    `);
+
+}
