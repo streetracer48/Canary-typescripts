24745189b106e9d5accea9033e921debe41d5b7b
karim <karim@csit.fi>
2020-02-26 17:39:03 +0200

    basic of interface added

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var ID = todo.ID;
+    var title = todo.title;
+    var finished = todo.finished;
+    console.log("The todo with TD:" + ID + "\n    Has a title of:" + title + "\n    Is it finished ? " + finished + "\n    ");
 });
--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,6 +3,21 @@ import axios from "axios"
 
 const url ='https://jsonplaceholder.typicode.com/todos/1'
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(response =>{
-    console.log(response.data)
+    const todo = response.data as Todo
+
+    const ID = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`The todo with TD:${ID}
+    Has a title of:${title}
+    Is it finished ? ${completed}
+    `)
 })
